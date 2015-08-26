9d25edbe26e1b76c67f86a4696768a9cc314baf9
James Franger <jfranger@gmail.com>
2015-08-26 06:52:42 +0000

    Added checking to see if an entry already exists in the spreadsheet

--- parse_rsvps.js
+++ parse_rsvps.js
@@ -21,6 +21,25 @@ if(LOGGING_ENABLED){
 }
 
 /*
+ * Set up our RSVP class that stores the details of the rsvp
+ */
+function RSVP(){
+
+	this.property_order = ['date', 'name', 'email', 'number_of_guests', 'events', 'can_attend'];
+
+	this.toArray = function(){
+		var arr = [];
+		for(i = 0; i < this.property_order.length; i++){
+			if(this.hasOwnProperty(this.property_order[i])){
+				arr.push(this[this.property_order[i]]);
+			}
+		}
+		return arr;
+	}
+
+}
+
+/*
  * Function that retrieves a list of gmail email threads
  * with a provided label and parses them.
  * @param - {string} rsvp_label - The label applied to emails that should be parsed.
@@ -75,9 +94,10 @@ function processRSVPs(rsvp_label) {
 function addRSVPToSpreadsheet(rsvp){
 
 	//Convert the rsvp object into an array
-	rsvp_array = Object.keys(rsvp).map(function(k) { return rsvp[k] });
-
+	rsvp_array = rsvp.toArray();
+	Logger.log('rsvp_array: ' + JSON.stringify(rsvp_array));
 	//Check if the spreadsheet exists or create a new one
+
 	var spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
 	var sheet = spreadsheet.getSheets()[0];
 
@@ -85,16 +105,72 @@ function addRSVPToSpreadsheet(rsvp){
 		return false;
 	}
 
+	if(RSVPEntryExists(rsvp, sheet)){
+		Logger.log('Entry already found in spreadsheet: ' + JSON.stringify(rsvp));
+		return false;
+	}
+
 	sheet = sheet.appendRow(rsvp_array);
 
-	if(!sheet){
-		return false;
-	}else{
-		return true;
-	}
+	return true;
 
 }
 
+function getSpreadsheetHeaderArray(sheet){
+
+	//Find the column index of the last row
+	var last_column = sheet.getLastColumn();
+
+	//Sheet.getRange(row, column, numRows, numColumns)
+	//Get all of the values from the first row
+	var range = sheet.getRange(1, 1, last_column, 1);
+	var values = range.getValues();
+
+	var header = [];
+	for (var row in values) {
+		for (var col in values[row]) {
+			header.push(values[row][col]);
+		}
+	}
+
+	Logger.log('header values: ' + JSON.stringify(header));
+	return header;
+
+}
+
+function RSVPEntryExists(rsvp, sheet){
+
+	var sheet_header = getSpreadSheetHeaderArray(sheet);
+
+	//Find the column that we need to be searching
+
+	search_column = sheet_header.indexOf('name');
+
+	if(search_column == -1){
+		//Flag the entry as existing since there was an error.
+		return true;
+	}
+
+	var last_row = sheet.getLastRow();
+
+	// getRange(row, column, numRows)
+	//Get all of the values from the search column for all rows
+	//(Add 1 to the search_column as the Spreadsheet is 1-indexed instead of 0-indexed)
+	var range = sheet.getRange(1, search_column + 1, last_row);
+
+	var values = range.getValues();
+
+	for (var row in values) {
+		for (var col in values[row]) {
+			if(values[row][col] == rsvp.name){
+				return true;
+			}
+		}
+	}
+
+	return false;
+
+}
 
 /*
  * Filters an array of messages and returns a modified version of the original array
@@ -143,7 +219,7 @@ function parseRSVPEmail(body) {
 	var message_lines = body.split("\n");
 
 	//Initialize our RSVP Object
-	rsvp = {};
+	rsvp = new RSVP();
 
 	var skip_fields = ['from', 'subject']
 
@@ -155,10 +231,10 @@ function parseRSVPEmail(body) {
 		// var line_type = line_parts[0].toLowerCase().replaceAll(' ', '_').replaceAll('/[^a-z0-9_]/', '');
 		var line_type = line_parts[0].toLowerCase();
 		line_type = replaceAll(' ', '_', line_type);
-		line_type = replaceAll('/[^a-z0-9_]/', '', line_type);
+		line_type = replaceAll('[^a-z0-9_]', '', line_type);
 		
 		//Skip over fields we don't need
-		if(skip_fields.indexOf(line_type) == -1){
+		if(skip_fields.indexOf(line_type) != -1){
 			continue;
 		}
 
@@ -167,6 +243,11 @@ function parseRSVPEmail(body) {
 
 		for(var j = 1; j < line_parts.length; j++) {
 			line_value += line_parts[j];
+		}
+
+		if(line_type.indexOf('number') != -1){
+			Logger.log('line_type: ' + line_type);
+			Logger.log('line_value: ' + line_value)
 		}
 		
 		line_value = line_value.trim();
@@ -213,7 +294,7 @@ function getGmailThreadsWithLabelName(label_name) {
 
 	if(LOGGING_ENABLED){
 		for (var i = 0; i < threads.length; i++) {
-			Logger.log(threads[i].getFirstMessageSubject());
+			// Logger.log(threads[i].getFirstMessageSubject());
 		}
 	}
 	
@@ -232,7 +313,7 @@ function getGmailThreadsWithLabelName(label_name) {
 // }
 
 function replaceAll(find, replace, subject){
-	return subject.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+	return subject.replace(new RegExp(find, 'g'), replace);
 }
 
 /*
